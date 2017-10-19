8fc42e5b8b4959aa459ce59f5a30bb7f6c9e53a8
Bryan Alvarado <bryan@atiko7.com>
2017-10-19 16:21:13 -0500

    discover it works with MidiMock

--- js/webmidi.js
+++ js/webmidi.js
@@ -1,5 +1,5 @@
 WebMidi.enable(function (err) {
-    var controllerId = "528718670";
+    //var controllerId = "528718670";
     if (err) {
     console.log("WebMidi could not be enabled.", err);
     } else {
@@ -9,62 +9,63 @@ WebMidi.enable(function (err) {
     console.log(WebMidi.inputs);
     console.log(WebMidi.outputs);
 
-    var output = WebMidi.getOutputById(controllerId);
+    //var output = WebMidi.getOutputById(controllerId);
+    var output = WebMidi.outputs[0];
    
-
+    //works with midimock
     console.log(WebMidi.time);
     
     
       // Play a note on all channels of the selected output
-      output.playNote("C3");
+      //output.playNote("C3");
     
       // Play a note on channel 3
-      output.playNote("Gb4", 3);
+      //output.playNote("Gb4", 3);
     
       // Play a chord on all available channels
-      output.playNote(["C3", "D#3", "G3"]);
+      output.playNote(["C2", "G2"]);
     
       // Play a chord on channel 7
-      output.playNote(["C3", "D#3", "G3"], 7);
+      //output.playNote(["C3", "D#3", "G3"], 7);
     
       // Play a note at full velocity on all channels)
-      output.playNote("F#-1", "all", {velocity: 1});
+      //output.playNote("F#-1", "all", {velocity: 1});
     
       // Play a note on channel 16 in 2 seconds (relative time)
-      output.playNote("F5", 16, {time: "+2000"});
+      //output.playNote("F5", 16, {time: "+2000"});
     
       // Play a note on channel 1 at an absolute time in the future
-      output.playNote("F5", 16, {time: WebMidi.time + 3000});
+      //output.playNote("F5", 16, {time: WebMidi.time + 3000});
     
       // Play a note for a duration of 2 seconds (will send a note off message in 2 seconds). Also use
       // a low attack velocity
-      output.playNote("Gb2", 10, {duration: 2000, velocity: 0.25});
+      //output.playNote("Gb2", 10, {duration: 2000, velocity: 0.25});
     
       // Stop a playing note on all channels
-      output.stopNote("C-1");
+      //output.stopNote("C-1");
     
       // Stopping a playing note on channel 11
-      output.stopNote("F3", 11);
+      //output.stopNote("F3", 11);
     
       // Stop a playing note on channel 11 and use a high release velocity
-      output.stopNote("G8", 11, {velocity: 0.9});
+      //output.stopNote("G8", 11, {velocity: 0.9});
     
       // Stopping a playing note in 2.5 seconds
-      output.stopNote("Bb2", 11, {time: "+2500"});
+      //output.stopNote("Bb2", 11, {time: "+2500"});
     
       // Send polyphonic aftertouch message to channel 8
-      output.sendKeyAftertouch("C#3", 8, 0.25);
+      //output.sendKeyAftertouch("C#3", 8, 0.25);
     
       // Send pitch bend (between -1 and 1) to channel 12
-      output.sendPitchBend(-1, 12);
+      //output.sendPitchBend(-1, 12);
     
       // You can chain most method calls
-      output.playNote("G5", 12)
-        .sendPitchBend(-0.5, 12, {time: 400}) // After 400 ms.
-        .sendPitchBend(0.5, 12, {time: 800})  // After 800 ms.
-        .stopNote("G5", 12, {time: 1200});    // After 1.2 s.
+      //output.playNote("G5", 12)
+        //.sendPitchBend(-0.5, 12, {time: 400}) // After 400 ms.
+        //.sendPitchBend(0.5, 12, {time: 800})  // After 800 ms.
+        //.stopNote("G5", 12, {time: 1200});    // After 1.2 s.
     
-      input = WebMidi.getInputById(controllerId);
+     /* input = WebMidi.getInputById(controllerId);
     
       // Listen for a 'note on' message on all channels
       input.addListener('noteon', "all",
@@ -103,5 +104,5 @@ WebMidi.enable(function (err) {
       input.removeListener('noteoff');
     
       // Remove all listeners on the input
-      input.removeListener();
+      input.removeListener();*/
 });
